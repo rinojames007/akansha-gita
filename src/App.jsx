b68062e08155eda454931f7856174a1d313bc669
Aryank35 <aryan5935kishan@gmail.com>
2023-11-18 02:16:00 +0530

    Navbar (Responsive)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./Components/Navbar/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-red-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  // State to manage the visibility of the mobile menu
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className="h-[50px] sm:h-[80px] w-full  flex justify-between items-center px-1 py-3 sm:px-5 border-2">
+      {/* Logo */}
+      <div className="logo h-[40px] w-[160px] sm:h-[65px] sm:w-[260px] rounded-full bg-gray-600 flex justify-center items-center hover:cursor-pointer">
+        <div className="header flex justify-center items-center">
+          <h1 className="text-xl font-semibold sm:text-2xl text-white">
+            Akanksha
+          </h1>
+        </div>
+      </div>
+      {/* navigations */}
+      <div className="nav-contents hidden sm:flex items-center">
+        <ul className="flex items-center justify-center sm:space-x-4 xl:space-x-6 text-xl font-bold">
+          <li className="hover:underline hover:text-gray-400 hover:cursor-pointer">Events</li>
+          <li className="hover:underline hover:text-gray-400 hover:cursor-pointer">Workshops</li>
+          <li className="hover:underline hover:text-gray-400 hover:cursor-pointer">Coordinators</li>
+          <li className="flex justify-center items-center">
+            <a
+              href="#"
+              className="mx-auto px-3 flex justify-center font-bold border-slate-600 border-2 rounded-2xl text-black hover:bg-slate-800 hover:text-white hover:cursor-pointer hover:transition-all hover:ease-in-out hover:duration-700 hover:delay-75"
+            >
+              <button className="px-1 py-2 text-center ">
+                Login/Signup
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Side-Menu (mobile option) */}
+      <div
+        className="side-menu sm:hidden "
+        id="MenuBar"
+        onClick={toggleMobileMenu}
+      >
+        <img src="./sidebar.png" alt="munu-bar" className="h-5 px-2" />
+      </div>
+
+      <div
+        className={`Mobile-Menu bg-slate-100 transition-all ease-out ${
+          isMobileMenuOpen ? "right-0" : "-right-[100%]"
+        }   absolute top-0 flex flex-col bg-[#fff] w-[60%] rounded-10 space-y-4 delay-200 shadow-sm shadow-blue-300 px-8 py-16 text-lg font-semibold h-fit rounded-xl`}
+      >
+        <div
+          className="cancel font-bold w-fit text-3xl relative -top-8 left-[2%]"
+          onClick={toggleMobileMenu}
+        >
+          <img src="./cross.png" alt="cross-icon" className="h-[30px]" />
+        </div>
+        <ul className="nav-menu flex flex-col items-center space-y-3 font-semibold ">
+          <li>
+            <a
+              className="hover:text-gray-400 active:underline py-1"
+              href="#"
+              onClick={toggleMobileMenu}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-gray-400 active:underline py-1"
+              href="#"
+              onClick={toggleMobileMenu}
+            >
+              Events
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-gray-400 active:underline py-1"
+              href="#"
+              onClick={toggleMobileMenu}
+            >
+              Workshops
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-gray-400 active:underline py-1"
+              href="#"
+              onClick={toggleMobileMenu}
+            >
+              Coordinators
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="mx-auto px-5 flex justify-center font-semibold bg-slate-800 border-slate-600 border-2 rounded-2xl text-white hover:bg-purple-400 hover:text-black hover:transition-all hover:ease-in-out hover:duration-700 hover:delay-75 mb-4"
+              onClick={toggleMobileMenu}
+            >
+              <button className="px-1 py-2 text-sm text-center ">
+                Login/Signup
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
