e5bb9f52a1fff8f4307db9a05546b431204a96b5
Aryank35 <aryan5935kishan@gmail.com>
2023-11-19 22:08:09 +0530

    tailwind css changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({images}) => {
     <div className="flex flex-col sm:flex-row h-fit w-full border-2 sm:space-x-3">
       {/* left-contents */}
       <div className="left w-full sm:w-[40%] mx-auto flex justify-center items-center ">
-        <div className="container flex flex-col justify-center items-center py-[20px] px-[30px] sm:py-[25%]">
+        <div className="container flex flex-col justify-center items-center py-[20px] px-2 sm:px-[30px] sm:py-[25%]">
           <div className="header">
             <h1 className=" text-2xl sm:text-4xl font-bold">Welcome to Akanksha</h1>
             <span className=" text-2xl sm:text-4xl font-bold">2024!</span>
